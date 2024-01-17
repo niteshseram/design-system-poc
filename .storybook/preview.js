a4d55187a7e86d1ab61425da6a4f4138b25aad85
Nitesh Seram <niteshseram@gmail.com>
2024-01-17 17:50:08 +0530

    Storybook setup

---
+++ .storybook/preview.js
@@ -0,0 +1,16 @@
+import "@auzmor-test/design-tokens/brand/auzmor/tokens.css";
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
---
+++ packages/ui/react/src/components/Button/Button.jsx
@@ -0,0 +1,37 @@
+import { clsx } from 'clsx';
+import { forwardRef } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Button = forwardRef(
+  ({ children, type = 'button', className, ...rest }, ref) => (
+    <StyledButton {...rest} ref={ref} type={type} className={clsx(className)}>
+      {children}
+    </StyledButton>
+  ),
+);
+
+export default Button;
+
+Button.displayName = 'Button';
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  type: PropTypes.string,
+  className: PropTypes.string,
+};
+
+Button.defaultProps = {
+  type: 'button',
+  className: '',
+};
+
+const StyledButton = styled.button`
+  background-color: var(--colorsPrimary950);
+  border-radius: var(--aldo2);
+  font: var(--textBaseFontMedium);
+  border: none;
+  color: var(--colorsBaseWhite);
+  font: var(--textBaseFontMedium);
+  cursor: pointer;
+`;
