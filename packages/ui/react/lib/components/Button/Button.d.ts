5f7fcc20dc42e3d3fc2ed29a7b41a45218a5ec20
Shubhdeep Chhabra <chhabrashubhdeep@gmail.com>
2024-01-18 15:19:53 +0530

    updated gitignore

--- packages/ui/react/lib/components/Button/Button.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-export declare const Button: import("react").ForwardRefExoticComponent<{
-    children: React.ReactNode;
-    type: string;
-    className: string;
-} & import("react").RefAttributes<unknown>>;
