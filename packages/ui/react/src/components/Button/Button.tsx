550b63651520a328dfc967ab2f6f08dc6ccea639
Shubhdeep Chhabra <chhabrashubhdeep@gmail.com>
2024-01-19 11:58:27 +0530

    updated button

--- packages/ui/react/src/components/Button/Button.tsx
+++ packages/ui/react/src/components/Button/Button.tsx
@@ -12,7 +12,7 @@ export const Button = forwardRef(
     }: { children: React.ReactNode; type: string; className: string },
     ref
   ) => (
-    <StyledButton buttonType={type} ref={ref} className={clsx(className)} {...rest}>
+    <StyledButton buttontype={type} ref={ref} className={clsx(className)} {...rest}>
       {children}
     </StyledButton>
   )
@@ -20,11 +20,11 @@ export const Button = forwardRef(
 
 Button.displayName = 'Button';
 
-const StyledButton = styled.button<{ buttonType: string }>`
+const StyledButton = styled.button<{ buttontype: string }>`
   background-color: var(--colorsPrimary950);
   border-radius: var(--aldo2);
   font: var(--textBaseFontMedium);
-  border: ${({ buttonType }) => (buttonType === 'outline' ? '1px solid' : 'none')};
+  border: ${({ buttontype }) => (buttontype === 'outline' ? '1px solid #000' : 'none')};
   color: var(--colorsBaseWhite);
   font: var(--textBaseFontMedium);
   cursor: pointer;
